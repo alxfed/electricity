59b10b5c045b7020949363a4a007941537233659
Alex Fedotov <alxfed@gmail.com>
2025-03-15 14:14:40 -0500

    initial

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ LICENSE file in the root directory of this source tree.
 from requests import get
 from requests.exceptions import RequestException
 import datetime
+import pytz
 
 
 def get_price():
@@ -15,7 +16,14 @@ def get_price():
         response = get('https://hourlypricing.comed.com/api?type=5minutefeed&format=json')
         prices = response.json()
         price24 = sum(float(price['price']) for price in prices) / len(prices)
-        lastprice = prices[-1]
+        seconds_UTC = int(int(prices[-1]['millisUTC'])/1000)
+        dt_utc = datetime.datetime.fromtimestamp(seconds_UTC, tz=pytz.UTC)
+        cst_tz = pytz.timezone('US/Central')
+        lasttime = dt_utc.astimezone(cst_tz)
+        # lasttime = datetime.datetime.strptime(prices[-1]['millisUTC'], '%Y-%m-%d %H:%M:%S')
+        m1 = prices[-2]['price']
+        m2 = prices[-3]['price']
+        lastprice = prices[-1]['price']
         hourprice = sum(float(price['price']) for price in prices[-12:]) / 12
         return price24, hourprice, lastprice
     except RequestException:
